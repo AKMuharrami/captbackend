0e1eda8b5fa13e1f4e62f497b56828c64572127e
Asad Al Muharrami <akmuharrami@gmail.com>
2026-04-28 23:16:04 +0400

    AA

--- server.ts
+++ server.ts
@@ -21,7 +21,8 @@ const app = express();
 const PORT = process.env.PORT || 3001; // Can run on any port e.g., Railway gives you PORT
 
 app.use(cors());
-app.use(express.json({limit: "50mb", extended: true} as any));
+app.options('*', cors());
+app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(express.text({ limit: '200mb' }));
 
